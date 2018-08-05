0dca53e7155bb612809ea42c682b5be86664a5e0
Benard18 <benkaranja43@gmail.com>
2018-08-05 21:53:11 +0300

    adding the necessary requirements based on the activation email in authentication

--- hood/settings.py
+++ hood/settings.py
@@ -139,3 +139,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+ACCOUNT_ACTIVATION_DAYS = 7
+SITE_ID = 3
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
