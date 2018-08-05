f73a1f2d64d8191a5be08513dc440c10b126b8f0
Benard18 <benkaranja43@gmail.com>
2018-08-05 17:51:00 +0300

    This is the Django GIS application based on locations and other uses to my neighbor_hood application

--- hood/settings.py
+++ hood/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'neigbor_hood',
+    'django.contrib.gis',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
