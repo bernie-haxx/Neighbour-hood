3da90ea81eba80fcd9186b09f199a603154ff1d1
Benard18 <benkaranja43@gmail.com>
2018-08-06 01:47:21 +0300

    updated the necessary imports based on the geojson

--- neigbor_hood/models.py
+++ neigbor_hood/models.py
@@ -1,9 +1,10 @@
-from django.db import models
+from django.contrib.gis.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from tinymce.models import HTMLField
-from django.contrib.gis.db import models as modelss
+from djgeojson.fields import PointField
+from jsonfield import JSONField
 # Create your models here.
 
 class Neighbor_hood(models.Model):
