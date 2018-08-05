4d557b32f079632f66eac45a0ff7e97169633c1d
Benard18 <benkaranja43@gmail.com>
2018-08-05 17:55:43 +0300

    This is the neighbor_hood class based on it meta data and also to base it as a blue print i my application

--- neigbor_hood/models.py
+++ neigbor_hood/models.py
@@ -4,3 +4,20 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from tinymce.models import HTMLField
 from django.contrib.gis.db import models as modelss
+# Create your models here.
+
+class Neighbor_hood(models.Model):
+	name=models.CharField(max_length=100, null=True, blank=True)
+	location=modelss.PointField()
+	def __str__(self):
+		return self.name
+	class Meta:
+		ordering = ['name']	
+
+	def save_hood(self):
+		self.save()	
+
+	@classmethod
+	def find_neighbor_hood(cls,hood_id):
+		hood=cls.objects.filter(id=hood_id)
+		return hood	
