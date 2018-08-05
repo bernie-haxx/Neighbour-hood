7fbff409d9b8397672543a6e7eb640b4499b6b74
Benard18 <benkaranja43@gmail.com>
2018-08-05 17:52:19 +0300

    removing the postgresql database backend

--- hood/settings.py
+++ hood/settings.py
@@ -77,14 +77,6 @@ WSGI_APPLICATION = 'hood.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-           'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           'NAME': 'hood',
-           'USER': 'ben',
-           'PASSWORD': 'ben',
-           'HOST': 'localhost',
-           'PORT': '',
-       }
 
 }
 
