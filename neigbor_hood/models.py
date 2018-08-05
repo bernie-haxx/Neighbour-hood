6adc4b48532ed8d9ae816f803f5cb2d0998dda95
Benard18 <benkaranja43@gmail.com>
2018-08-05 17:57:15 +0300

    This is the users class that has the necessary things to enable the user to have ceratin privileges and also to link the auth user with the my user profiles

--- neigbor_hood/models.py
+++ neigbor_hood/models.py
@@ -21,3 +21,28 @@ class Neighbor_hood(models.Model):
 	def find_neighbor_hood(cls,hood_id):
 		hood=cls.objects.filter(id=hood_id)
 		return hood	
+
+class UserProfile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE,related_name="profile")
+	ID=models.CharField(max_length=100, null=True, blank=True)
+	profilepicture = models.ImageField(upload_to='images/', blank=True,default="/black.png")
+	neighbor_hood=models.ForeignKey(Neighbor_hood, on_delete=models.CASCADE,related_name="hood",null=True,blank=True)
+	secondary_email = models.CharField(max_length=100, null=True, blank=True)
+
+	def __str__(self):
+		return self.user
+	class Meta:
+		ordering = ['user']
+	def save_user(self):
+		self.save()
+	def delete_user(self):
+		self.delete()		
+
+	@receiver(post_save, sender=User)
+	def create_user_profile(sender, instance, created, **kwargs):
+		if created:
+				UserProfile.objects.create(user=instance)
+
+	@receiver(post_save, sender=User)
+	def save_user_profile(sender, instance, **kwargs):
+		instance.profile.save()
