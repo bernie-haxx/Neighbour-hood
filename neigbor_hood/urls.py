8631b5d2ef7f3ab4ad6c1a92766d45bcc968f64a
Benard18 <benkaranja43@gmail.com>
2018-08-06 01:48:29 +0300

    adding new urls based on the application

--- neigbor_hood/urls.py
+++ neigbor_hood/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from .  import views
 
 urlpatterns=[
-url(r'^$',views.home,name='home')
+url(r'^$',views.home,name='home'),
+url(r'^new_profile/$',views.new_profile,name='new_profile')
 ]
