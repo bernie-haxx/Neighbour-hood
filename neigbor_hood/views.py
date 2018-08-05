88f57372e7f5ed2f641c3c7860a9ef8242f61d45
Benard18 <benkaranja43@gmail.com>
2018-08-06 01:48:47 +0300

    adding the new_profile function

--- neigbor_hood/views.py
+++ neigbor_hood/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from .forms import NewProfileForm
 # Create your views here.
 @login_required(login_url='accounts/login')
 def home(request):
 	return render(request,'home.html')
+
+@login_required(login_url='/accounts/login')
+def new_profile(request):
+	current_user = request.user
+	if request.method == 'POST':
+		form = NewProfileForm(request.POST,request.FILES, instance=request.user.profile)
+		if form.is_valid():
+			form.save()
+			return redirect('home')
+
+	else:
+			form = NewProfileForm()
+	return render(request, 'new_profile.html',{"form":form })
+
