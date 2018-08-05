296361dbe8855b54891982433feab86cbdeb3d21
Benard18 <benkaranja43@gmail.com>
2018-08-05 17:54:04 +0300

    making changes in the admin.py so as to enable the application to have an admin dashboard

--- neigbor_hood/admin.py
+++ neigbor_hood/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import UserProfile,Neighbor_hood,Business
 # Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Neighbor_hood)
+admin.site.register(Business)
