b03a84210990a9285f02be757d3728bf46fb026a
Benard18 <benkaranja43@gmail.com>
2018-08-06 01:44:12 +0300

    addin gthe necessary leaflet imports so as to enable display of the maps either in admin dashboard or in html

--- neigbor_hood/admin.py
+++ neigbor_hood/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import UserProfile,Neighbor_hood,Business
+from leaflet.admin import LeafletGeoAdmin
+from leaflet.admin import LeafletGeoAdminMixin
 # Register your models here.
 admin.site.register(UserProfile)
 admin.site.register(Neighbor_hood)
