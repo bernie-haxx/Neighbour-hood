9683acbd3074691c89b09a4129bbbcf19d6fa9db
Benard18 <benkaranja43@gmail.com>
2018-08-09 10:32:07 +0300

    adding the post and comments to the admin site

--- neigbor_hood/admin.py
+++ neigbor_hood/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import UserProfile,Neighbor_hood,Business
+from .models import UserProfile,Neighbor_hood,Business,Post,Comments
 from leaflet.admin import LeafletGeoAdmin
 from leaflet.admin import LeafletGeoAdminMixin
 # Register your models here.
 admin.site.register(UserProfile)
 admin.site.register(Neighbor_hood, LeafletGeoAdmin)
 admin.site.register(Business)
+admin.site.register(Post)
+admin.site.register(Comments)
+
