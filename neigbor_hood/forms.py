15097f037be6892a00d0e10a29637542a1537458
Benard18 <benkaranja43@gmail.com>
2018-08-09 10:32:50 +0300

    adding the necessary imports based on the forms.py

--- neigbor_hood/forms.py
+++ neigbor_hood/forms.py
@@ -2,9 +2,10 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from floppyforms.gis import PointWidget, BaseGMapWidget
-from .models import UserProfile,Neighbor_hood,Business
-
-
+from .models import UserProfile,Neighbor_hood,Business,Post,Comments
+from django.contrib import admin
+from leaflet.admin import LeafletGeoAdminMixin
+from leaflet.forms.widgets import LeafletWidget
 class NewProfileForm(forms.ModelForm):
 	class Meta:
 		model = UserProfile
