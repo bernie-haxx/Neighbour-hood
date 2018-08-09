f674ac5972c31756e323566dc639e1e57a6ce3a7
Benard18 <benkaranja43@gmail.com>
2018-08-09 10:27:07 +0300

    placing the databases based with heroku_django

--- hood/settings.py
+++ hood/settings.py
@@ -99,16 +99,28 @@ WSGI_APPLICATION = 'hood.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-   	'default': {
-       'ENGINE': 'django.contrib.gis.db.backends.postgis',
-       'NAME': 'hood',
-       'USER': 'ben',
-       'PASSWORD': 'ben',
-       'HOST': 'localhost',
-       'PORT': '',
+if config('MODE')=="dev":
+   DATABASES = {
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': config('DB_NAME'),
+           'USER': config('DB_USER'),
+           'PASSWORD': config('DB_PASSWORD'),
+           'HOST': config('DB_HOST'),
+           'PORT': '',
+       }
+       
+   }
+# production
+else:
+   DATABASES = {
+       'default': dj_database_url.config(
+           default=config('DATABASE_URL')
+       )
    }
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 }
 
