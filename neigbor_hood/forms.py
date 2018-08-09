f56d5d03e9b6d444d14330b1c99db1610bee36e7
Benard18 <benkaranja43@gmail.com>
2018-08-09 10:33:25 +0300

    adding the neighbor_hood classes,comments,post forms to the application

--- neigbor_hood/forms.py
+++ neigbor_hood/forms.py
@@ -10,3 +10,22 @@ class NewProfileForm(forms.ModelForm):
 	class Meta:
 		model = UserProfile
 		exclude = ['user'] 
+class NewNeigbor_hood_Form(forms.ModelForm):
+	class Meta:
+		model = Neighbor_hood
+		fields = ['name','location']
+
+class New_Business_Form(forms.ModelForm):
+	class Meta:
+		model = Business
+		exclude = ['user'] 		
+class NewPostForm(forms.ModelForm):
+	class Meta:
+		model = Post
+		exclude = ['user']
+
+
+class NewCommentForm(forms.ModelForm):	
+	class Meta:
+		model = Comments
+		exclude = ['user','date_posted','post']
