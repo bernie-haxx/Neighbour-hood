c8dc26a8815fd5dd17ec499cae5280983622d113
Benard18 <benkaranja43@gmail.com>
2018-08-09 10:27:32 +0300

    the necessary config vars

--- hood/settings.py
+++ hood/settings.py
@@ -122,7 +122,7 @@ else:
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-}
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
@@ -160,7 +160,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
